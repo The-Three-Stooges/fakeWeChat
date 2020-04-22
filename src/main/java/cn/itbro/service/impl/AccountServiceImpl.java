af131bff152ed97c840bf32bd093038861fb2c65
linyangxin <1227143051@qq.com>
2020-04-22 08:50:25 +0800

    smm框架整合完成

--- src/main/java/cn/itbro/service/impl/AccountServiceImpl.java
+++ src/main/java/cn/itbro/service/impl/AccountServiceImpl.java
@@ -1,7 +1,9 @@
 package cn.itbro.service.impl;
 
+import cn.itbro.dao.AccountDao;
 import cn.itbro.domain.Account;
 import cn.itbro.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,14 +11,19 @@ import java.util.List;
 
 @Service("accountService")
 public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
     @Override
     public List<Account> findAll() {
         System.out.println("业务层：查询所有账户信息。。。");
-        return null;
+        return accountDao.findAll();
     }
 
     @Override
     public void saveAccount(Account account) {
         System.out.println("业务层：保存账户。。。");
+        accountDao.saveAccount(account);
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 12271
+  Date: 2020/4/21
+  Time: 22:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>title</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+    <a href="account/findAll">测试</a>
+
+    <form action="/account/save" method="post">
+        姓名：<input type="text" name="name"><br>
+        金额：<input type="text" name="money"><br>
+        <input type="submit" value="保存"><br>
+    </form>
+
 </body>
 </html>
